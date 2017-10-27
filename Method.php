29e8f732b486126f3016f472514a3de803c9cd90
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-10-27 06:54:47 +0300

    no message

--- Method.php
+++ Method.php
@@ -3,6 +3,27 @@ namespace Dfe\AlphaCommerceHub;
 // 2017-10-25
 final class Method extends \Df\PaypalClone\Method {
 	/**
+	 * 2017-10-27
+	 * «The amount fields are defined as fixed format 14,3 length.
+	 * This allows for three exponent currencies such as OMR (Omani Rial) to be supported
+	 * as well as supporting consistent formatting of zero exponent currencies such as JPY (Japanese Yen)
+	 * as well as two exponent currencies such as USD (US Dollar).
+	 * A merchant when submitting an amount
+	 * should always submit assuming three minor units of the currency as is demonstrated:
+	 * Amount		Currency	Submit As
+	 * 250			JPY			250000
+	 * 250.99		EUR			250990
+	 * 250.999		OMR			250999»
+	 * http://alpha.pwstaging.com.au/docs/alphahpp#handling-amounts
+	 * @override
+	 * @see \Df\Payment\Method::amountFactor()
+	 * @used-by \Df\Payment\Method::amountFormat()
+	 * @used-by \Df\Payment\Method::amountParse()
+	 * @return int
+	 */
+	protected function amountFactor() {return 1000;}
+
+	/**
 	 * 2017-10-25
 	 * @override
 	 * @see \Df\Payment\Method::amountLimits()
