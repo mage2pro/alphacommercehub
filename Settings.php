cdd04562f78de652984fcf8de52d86046fd99d52
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-10-28 07:04:02 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -1,5 +1,14 @@
 <?php
 namespace Dfe\AlphaCommerceHub;
+use Df\Payment\Settings\Options as O;
+use Dfe\AlphaCommerceHub\Source\Option as OptionSource;
 // 2017-10-25
 /** @method static Settings s() */
-final class Settings extends \Df\Payment\Settings {}
+final class Settings extends \Df\Payment\Settings {
+	/**
+	 * 2017-10-28
+	 * @used-by \Dfe\AlphaCommerceHub\ConfigProvider::options()
+	 * @return O
+	 */
+	function options() {return $this->_options(OptionSource::class);}
+}
