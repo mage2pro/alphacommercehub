598369732714b11da38ea184478746461d942348
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-10-28 07:33:30 +0300

    0.0.5

---
+++ ConfigProvider.php
@@ -0,0 +1,24 @@
+<?php
+namespace Dfe\AlphaCommerceHub;
+use Df\Payment\ConfigProvider\IOptions;
+// 2017-10-28
+/** @method Settings s() */
+final class ConfigProvider extends \Df\Payment\ConfigProvider implements IOptions {
+	/**
+	 * 2017-10-28
+	 * @override
+	 * @see \Df\Payment\ConfigProvider\IOptions::options()
+	 * @used-by \Df\Payment\ConfigProvider::configOptions()
+	 * @return array(<value> => <label>)
+	 */
+	function options() {return $this->s()->options()->o(true);}
+
+	/**
+	 * 2017-10-28
+	 * @override
+	 * @see \Df\Payment\ConfigProvider::config()
+	 * @used-by \Df\Payment\ConfigProvider::getConfig()
+	 * @return array(string => mixed)
+	 */
+	protected function config() {return self::configOptions($this) + parent::config();}
+}
