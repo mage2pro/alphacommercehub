7bbb67d03874e7d29d3fa8e305fb31574e5653d2
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-11-01 11:41:38 +0300

    no message

--- Charge.php
+++ Charge.php
@@ -1,6 +1,9 @@
 <?php
 namespace Dfe\AlphaCommerceHub;
 use Df\Payment\Init\Action;
+use Df\Payment\Settings\Options;
+use Dfe\AlphaCommerceHub\Settings as S;
+use Magento\Sales\Model\Order\Address as OA;
 /**
  * 2017-10-27
  * The charge parameters are described
@@ -94,110 +97,124 @@ final class Charge extends \Df\PaypalClone\Charge {
 	 * @used-by \Df\PaypalClone\Charge::p()
 	 * @return array(string => mixed)
 	 */
-	protected function pCharge() {$s = $this->s(); $o = $s->options(); return [
+	protected function pCharge() {
 		/**
 		 * 2017-11-01
-		 * «Y/N – Allows a merchant to flag that a transaction should be captured (settled) if the auth is».
-		 * String(1), optional.
+		 * «Whether the `Street1`, `Street2`, `City`, `Zip`, `Country` parameters
+		 * are related to a shipping address, or to a billing address?» https://mage2.pro/t/4855
 		 */
-		'Capture' => Action::singleton($this->m())->preconfiguredToCapture() ? 'Y' : 'N'
-		/**
-		 * 2017-11-01
-		 * «A customer identifier assigned by the merchant». String(20), optional.
-		 * Should `CustomerID` be unique or not? https://mage2.pro/t/4854
-		 */
-		,'CustomerID' => $this->customerEmail()
-		// 2017-11-01 «The customers IP address». String(15), optional.
-		,'IPAddress' => df_visitor_ip()
-		/**
-		 * 2017-11-01
-		 * Note 1. «AlphaHPP» → «Paypage Request Reference» → «Request a Paypage Session»
-		 * «The language the customer selected on the merchants site.
-		 * For example `EN` for English, `DE` for German.
-		 * If not provided the page will default to English
-		 * or if a language is provided that is not supported the page will default to.»
-		 * String(2), optional.
-		 * http://developer.alphacommercehub.com.au/docs/alphahpp-#request-a-paypage-session
-		 * Note 2. «AlphaHPP» → «Paypage Request Reference» → «Supported Languages»
-		 * http://developer.alphacommercehub.com.au/docs/alphahpp-#supported-languages
-		 */
-		,'Language' => df_lang(df_visitor_locale())
-		/**
-		 * 2017-11-01
-		 * Note 1.
-		 * «Allows the merchant to specify a statement descriptor for the transaction.
-		 * Please note the narrative supported by different card schemes varies.»
-		 * http://alpha.pwstaging.com.au/docs/alphahpp#request-a-paypage-session
-		 * String(64), optional.
-		 * Note 2.
-		 * «[AlphaCommerceHub] Is the `MerchantDescriptor` parameter really accepts up to 64 characters?»
-		 * https://mage2.pro/t/4800
-		 * «The value is truncated depending on the provider of the payment processing.
-		 * The Wikipedia article mainly focuses on card schemes,
-		 * but as the platform supports many different payment methods worldwide
-		 * this can be passed through to various providers where appropriate.
-		 * The 64 character limit is simply what the hub will accept,
-		 * not what is sent through to the end provider.»
-		 * Note 3.
-		 * «Are any unallowed characters for the `MerchantDescriptor` parameter values,
-		 * and how does AlphaCommerceHub handle them
-		 * (silently strip them, or reject the payment API request at whole)?»
-		 * https://mage2.pro/t/4801
-		 * Note 4.  We provide also `TxnDetails` for a long description.
-		 */
-		,'MerchantDescriptor' => $this->text($s->dsd(), 64)
-		/**
-		 * 2017-10-27
-		 * Note 1.
-		 * «The payment method to be used, default to `CC`.
-		 * `ALL` will display all configured payment methods.
-		 * Merchant can select a subset by delimiting.
-		 * E.g. `CC,ID,AL` would display cards, iDeal, Alipay if supported by AlphaHPP.»
-		 * String(14), required.
-		 * Note 2. [AlphaCommerceHub] The payment method codes: https://mage2.pro/t/4809
-		 * Note 3.
-		 * [AlphaCommerceHub] Which codes are used for the
-		 * Visa Checkout, UnionPay Online Payments, and ApplePay payment options?
-		 * https://mage2.pro/t/4811
-		 *
-		 * 2017-11-01
-		 * I have implemented it similar to @see \Dfe\AllPay\Charge::pChoosePayment():
-		 * https://github.com/mage2pro/allpay/blob/1.10.0/Charge.php#L538-L562
-		 */
-		,'Method' => $this->m()->option() ?: (!$o->isLimited() ? 'ALL' : df_csv($o->allowed()))
-		// 2017-11-01 «A social or tax id for the customer». String(20), optional.
-		,'SocialID' => $this->customerVAT()
-		// 2017-11-01 «The customers phone number. Numbers only.» Numeric(6-14), optional.
-		,'TelNo' => substr(preg_replace('/\D/', '', $this->customerPhone()), 0, 14)
-		/**
-		 * 2017-10-27
-		 * «The date and time of the transaction.
-		 * If not provided defaults to ACH system timestamp (UTC).
-		 * Format: DDMMYYYYHHMMSS»
-		 * String(14), optional.
-		 */
-		,'TransactionTimestamp' => null
-		/**
-		 * 2017-11-01
-		 * Note 1. «Information on the product/service being purchased». String(2000), optional.
-		 * Note 2.
-		 * We provide also `MerchantDescriptor` for a very short description:
-		 * e.g., it is used as a dynamic statement descriptor for the bank card payment option:
-		 * https://en.wikipedia.org/wiki/Billing_descriptor#Dynamic_descriptors
-		 */
-		,'TxnDetails' => $this->description()
-		/**
-		 * 2017-10-27
-		 * Note 1. «User id for authentication», String, required.
-		 * Note 2. [AlphaCommerceHub] How is the APIs authentication security implemented? https://mage2.pro/t/4768
-		 * Note 3.
-		 * [AlphaCommerceHub] Will the `MerchantID` and `UserID` values be preserved
-		 * for a particular merchant after switching its account from the test mode to the production one?
-		 * https://mage2.pro/t/4798
-		 * Note 4. [AlphaCommerceHub] Where can a merchant find his `MerchantID` and `UserID` values?
-		 * https://mage2.pro/t/4799
-		 * Note 5. AlphaCommerceHub does not use signatures. `UserId` is really a merchant password.
-		 */
-		,'UserId' => $s->privateKey()
-	];}
+		/** @var OA $sa */$sa = $this->addressS();
+		/** @var S $s */ $s = $this->s();
+		/** @var Options $o */ $o = $s->options();
+		return [
+			/**
+			 * 2017-11-01
+			 * «Y/N – Allows a merchant to flag that a transaction should be captured (settled) if the auth is».
+			 * String(1), optional.
+			 */
+			'Capture' => Action::singleton($this->m())->preconfiguredToCapture() ? 'Y' : 'N'
+			/**
+			 * 2017-11-01
+			 * «A customer identifier assigned by the merchant». String(20), optional.
+			 * Should `CustomerID` be unique or not? https://mage2.pro/t/4854
+			 */
+			,'CustomerID' => $this->customerEmail()
+			// 2017-11-01 «The customers IP address». String(15), optional.
+			,'IPAddress' => df_visitor_ip()
+			/**
+			 * 2017-11-01
+			 * Note 1. «AlphaHPP» → «Paypage Request Reference» → «Request a Paypage Session»
+			 * «The language the customer selected on the merchants site.
+			 * For example `EN` for English, `DE` for German.
+			 * If not provided the page will default to English
+			 * or if a language is provided that is not supported the page will default to.»
+			 * String(2), optional.
+			 * http://developer.alphacommercehub.com.au/docs/alphahpp-#request-a-paypage-session
+			 * Note 2. «AlphaHPP» → «Paypage Request Reference» → «Supported Languages»
+			 * http://developer.alphacommercehub.com.au/docs/alphahpp-#supported-languages
+			 */
+			,'Language' => df_lang(df_visitor_locale())
+			/**
+			 * 2017-11-01
+			 * Note 1.
+			 * «Allows the merchant to specify a statement descriptor for the transaction.
+			 * Please note the narrative supported by different card schemes varies.»
+			 * http://alpha.pwstaging.com.au/docs/alphahpp#request-a-paypage-session
+			 * String(64), optional.
+			 * Note 2.
+			 * «[AlphaCommerceHub] Is the `MerchantDescriptor` parameter really accepts up to 64 characters?»
+			 * https://mage2.pro/t/4800
+			 * «The value is truncated depending on the provider of the payment processing.
+			 * The Wikipedia article mainly focuses on card schemes,
+			 * but as the platform supports many different payment methods worldwide
+			 * this can be passed through to various providers where appropriate.
+			 * The 64 character limit is simply what the hub will accept,
+			 * not what is sent through to the end provider.»
+			 * Note 3.
+			 * «Are any unallowed characters for the `MerchantDescriptor` parameter values,
+			 * and how does AlphaCommerceHub handle them
+			 * (silently strip them, or reject the payment API request at whole)?»
+			 * https://mage2.pro/t/4801
+			 * Note 4.  We provide also `TxnDetails` for a long description.
+			 */
+			,'MerchantDescriptor' => $this->text($s->dsd(), 64)
+			/**
+			 * 2017-10-27
+			 * Note 1.
+			 * «The payment method to be used, default to `CC`.
+			 * `ALL` will display all configured payment methods.
+			 * Merchant can select a subset by delimiting.
+			 * E.g. `CC,ID,AL` would display cards, iDeal, Alipay if supported by AlphaHPP.»
+			 * String(14), required.
+			 * Note 2. [AlphaCommerceHub] The payment method codes: https://mage2.pro/t/4809
+			 * Note 3.
+			 * [AlphaCommerceHub] Which codes are used for the
+			 * Visa Checkout, UnionPay Online Payments, and ApplePay payment options?
+			 * https://mage2.pro/t/4811
+			 *
+			 * 2017-11-01
+			 * I have implemented it similar to @see \Dfe\AllPay\Charge::pChoosePayment():
+			 * https://github.com/mage2pro/allpay/blob/1.10.0/Charge.php#L538-L562
+			 */
+			,'Method' => $this->m()->option() ?: (!$o->isLimited() ? 'ALL' : df_csv($o->allowed()))
+			// 2017-11-01 «A social or tax id for the customer». String(20), optional.
+			,'SocialID' => $this->customerVAT()
+			// 2017-11-01 «The first line of the customers street address». String(100), optional.
+			,'Street1' => $sa->getStreet(1)
+			// 2017-11-01 «The second line of the customers street address». String(100), optional.
+			,'Street2' => $sa->getStreet(2)
+			// 2017-11-01 «The customers phone number. Numbers only.» Numeric(6-14), optional.
+			,'TelNo' => substr(preg_replace('/\D/', '', $this->customerPhone()), 0, 14)
+			/**
+			 * 2017-10-27
+			 * «The date and time of the transaction.
+			 * If not provided defaults to ACH system timestamp (UTC).
+			 * Format: DDMMYYYYHHMMSS»
+			 * String(14), optional.
+			 */
+			,'TransactionTimestamp' => null
+			/**
+			 * 2017-11-01
+			 * Note 1. «Information on the product/service being purchased». String(2000), optional.
+			 * Note 2.
+			 * We provide also `MerchantDescriptor` for a very short description:
+			 * e.g., it is used as a dynamic statement descriptor for the bank card payment option:
+			 * https://en.wikipedia.org/wiki/Billing_descriptor#Dynamic_descriptors
+			 */
+			,'TxnDetails' => $this->description()
+			/**
+			 * 2017-10-27
+			 * Note 1. «User id for authentication», String, required.
+			 * Note 2. [AlphaCommerceHub] How is the APIs authentication security implemented? https://mage2.pro/t/4768
+			 * Note 3.
+			 * [AlphaCommerceHub] Will the `MerchantID` and `UserID` values be preserved
+			 * for a particular merchant after switching its account from the test mode to the production one?
+			 * https://mage2.pro/t/4798
+			 * Note 4. [AlphaCommerceHub] Where can a merchant find his `MerchantID` and `UserID` values?
+			 * https://mage2.pro/t/4799
+			 * Note 5. AlphaCommerceHub does not use signatures. `UserId` is really a merchant password.
+			 */
+			,'UserId' => $s->privateKey()
+		];
+	}
 }
