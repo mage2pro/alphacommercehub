65d0a4ab054eaf0ec1b26593efa4c5f2d51bd3d8
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-11-19 01:26:04 +0300

    0.3.4: https://github.com/mage2pro/alphacommercehub/issues/29

---
+++ Block/Info.php
@@ -0,0 +1,13 @@
+<?php
+namespace Dfe\AlphaCommerceHub\Block;
+// 2017-11-19
+/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+class Info extends \Df\Payment\Block\Info {
+	/**
+	 * 2017-11-19
+	 * @override
+	 * @see \Df\Payment\Block\Info::prepare()
+	 * @used-by \Df\Payment\Block\Info::_prepareSpecificInformation()
+	 */
+	final protected function prepare() {}
+}
