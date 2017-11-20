176edd930433246c28bc0eb468712fc1484c242a
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-11-20 17:51:48 +0300

    0.3.6: https://github.com/mage2pro/alphacommercehub/issues/41

--- Block/Info.php
+++ Block/Info.php
@@ -1,7 +1,11 @@
 <?php
 namespace Dfe\AlphaCommerceHub\Block;
-// 2017-11-19
-/** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
+use Dfe\AlphaCommerceHub\W\Event;
+/**
+ * 2017-11-19
+ * @final Unable to use the PHP «final» keyword here because of the M2 code generation.
+ * @method Event|string|null e(...$k)
+ */
 class Info extends \Df\Payment\Block\Info {
 	/**
 	 * 2017-11-19
@@ -9,5 +13,14 @@ class Info extends \Df\Payment\Block\Info {
 	 * @see \Df\Payment\Block\Info::prepare()
 	 * @used-by \Df\Payment\Block\Info::_prepareSpecificInformation()
 	 */
-	final protected function prepare() {}
+	final protected function prepare() {
+		$e = $this->e(); /** @var Event $e */
+		/**
+		 * 2017-11-20
+		 * "Show the AlphaCommerceHub's payment ID (`PaymentID`)
+		 * in the Magento's backend «Payment Information» block":
+		 * https://github.com/mage2pro/alphacommercehub/issues/41
+		 */
+		$this->siEx('AlphaCommerceHub ID', $e->idE());
+	}
 }
