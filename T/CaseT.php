63ada01966e81464a762633ca0d1ac9fe2ef4747
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-12-02 08:52:26 +0300

    https://github.com/mage2pro/alphacommercehub/issues/63

---
+++ T/CaseT.php
@@ -0,0 +1,8 @@
+<?php
+namespace Dfe\AlphaCommerceHub\T;
+/**
+ * 2017-12-02
+ * @see \Dfe\AlphaCommerceHub\T\CaseT\PayPal\PaymentStatus
+ * @method \Dfe\AlphaCommerceHub\Method m()
+ */
+abstract class CaseT extends \Df\Payment\TestCase {}
---
+++ T/CaseT/PayPal/PaymentStatus.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dfe\AlphaCommerceHub\T\CaseT\PayPal;
+// 2017-12-02
+final class PaymentStatus extends \Dfe\AlphaCommerceHub\T\CaseT {
+	/** @test 2017-12-02 */
+	function t00() {}
+}
