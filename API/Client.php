265c313a341363f4fe9b492e62632286640c0abc
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-12-02 05:28:50 +0300

    https://github.com/mage2pro/alphacommercehub/issues/63

--- API/Client.php
+++ API/Client.php
@@ -5,6 +5,19 @@ use Dfe\AlphaCommerceHub\Method as M;
 final class Client extends \Df\API\Client {
 	/**
 	 * 2017-12-02
+	 * «To distinguish whether the message content is sent as XML or JSON
+	 * please provide `Content-Type` as either `application/xml` or `application/json`»
+	 * http://developer.alphacommercehub.com.au/docs/api-integration-guidenov-2017#api-reference
+	 * @override
+	 * @see \Df\API\Client::headers()
+	 * @used-by \Df\API\Client::__construct()
+	 * @used-by \Df\API\Client::p()
+	 * @return array(string => string)
+	 */
+	protected function headers() {return ['Content-Type' => 'application/json'];}
+
+	/**
+	 * 2017-12-02
 	 * @override
 	 * @see \Df\API\Client::urlBase()
 	 * @used-by \Df\API\Client::__construct()
