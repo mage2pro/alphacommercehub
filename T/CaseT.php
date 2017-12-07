947168653cbbca6eb1146a25fc5143d05a94e071
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-12-08 00:27:46 +0300

    https://github.com/mage2pro/alphacommercehub/issues/69

--- T/CaseT.php
+++ T/CaseT.php
@@ -2,6 +2,7 @@
 namespace Dfe\AlphaCommerceHub\T;
 /**
  * 2017-12-02
+ * @see \Dfe\AlphaCommerceHub\T\CaseT\BankCard\CancelPayment()
  * @see \Dfe\AlphaCommerceHub\T\CaseT\BankCard\CapturePayment
  * @see \Dfe\AlphaCommerceHub\T\CaseT\BankCard\RefundPayment
  * @see \Dfe\AlphaCommerceHub\T\CaseT\PayPal\CapturePayment
---
+++ T/CaseT/BankCard/CancelPayment.php
@@ -0,0 +1,20 @@
+<?php
+namespace Dfe\AlphaCommerceHub\T\CaseT\BankCard;
+use Df\API\Operation as Op;
+use Dfe\AlphaCommerceHub\API\Facade as F;
+/**
+ * 2017-12-08
+ * "Implement an ability to void a preauthorized bank card payment from the Magento backend
+ * (the `CancelPayment` transaction)" https://github.com/mage2pro/alphacommercehub/issues/69
+ */
+final class CancelPayment extends \Dfe\AlphaCommerceHub\T\CaseT {
+	/** @test 2017-12-08 */
+	function t00() {}
+
+	/** 2017-12-08 */
+	function t01() {
+		/** @var Op $r */
+		$r = F::s()->post(['Transaction' => ['MerchantTxnID' => '1206L.763']], df_class_l($this));
+		echo $r->j();
+	}
+}
