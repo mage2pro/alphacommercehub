8b78ceca2ac49dec90241281ba7dc0017bd5c976
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-12-07 10:26:37 +0300

    https://github.com/mage2pro/alphacommercehub/issues/60

--- T/CaseT/BankCard/CapturePayment.php
+++ T/CaseT/BankCard/CapturePayment.php
@@ -6,12 +6,25 @@ use Dfe\AlphaCommerceHub\API\Facade as F;
  * 2017-12-06
  * "Implement an ability to capture a preauthorized bank card payment from the Magento backend
  * (the `CapturePayment` transaction)": https://github.com/mage2pro/alphacommercehub/issues/60
+ * 2017-12-07
+ * 1) "A `CapturePayment` API request for a preauthorized bank card payment, and a response to it":
+ * https://mage2.pro/t/5107
+ * 2) "Why does AlphaCommerceHub allow to capture more money from a customer than it was preauthorized?"
+ * https://mage2.pro/t/5104
+ * 3) "Why does AlphaCommerceHub allow to capture zero amounts?" https://mage2.pro/t/5105
+ * 4) "Why does AlphaCommerceHub allow to capture some money without a currency specified?"
+ * https://mage2.pro/t/5106
+ * 5) "Is it possible to capture a preauthorized bank card payment partially?" https://mage2.pro/t/5102
+ * 6) "Is it possible to capture a preauthorized bank card payment partially multiple times?"
+ * https://mage2.pro/t/5103
+ * 7) "Neither the documentation nor the API Explorer provides a sample of the `CapturePayment` transaction
+ * for a bank card payment" https://mage2.pro/t/5075
  */
 final class CapturePayment extends \Dfe\AlphaCommerceHub\T\CaseT {
-	/** @test 2017-12-06 */
+	/** 2017-12-06 */
 	function t00() {}
 
-	/** 2017-12-06 */
+	/** @test 2017-12-06 */
 	function t01() {
 		$r = F::s()->post(['Transaction' => [
 			/**
@@ -21,7 +34,7 @@ final class CapturePayment extends \Dfe\AlphaCommerceHub\T\CaseT {
 			 * «API Integration Guide(Nov 2017)» → «API Reference» → «Request Message»
 			 * http://developer.alphacommercehub.com.au/docs/api-integration-guidenov-2017#request-message-
 			 */
-			'Amount' => '161000'
+			'Amount' => '999999' //'161000'
 			/**
 			 * 2017-12-06
 			 * «ISO 4217 Currency Code e.g. USD/GBP/EUR»
@@ -37,7 +50,7 @@ final class CapturePayment extends \Dfe\AlphaCommerceHub\T\CaseT {
 			 * «API Integration Guide(Nov 2017)» → «API Reference» → «Request Message»
 			 * http://developer.alphacommercehub.com.au/docs/api-integration-guidenov-2017#request-message-
 			 */
-			,'MerchantTxnID' => '1203L.760'
+			,'MerchantTxnID' => '1206L.767'
 			/**
 			 * 2017-12-06
 			 * «The payment method used. See Payment Methods.»
@@ -45,7 +58,9 @@ final class CapturePayment extends \Dfe\AlphaCommerceHub\T\CaseT {
 			 * «API Integration Guide(Nov 2017)» → «API Reference» → «Request Message»
 			 * http://developer.alphacommercehub.com.au/docs/api-integration-guidenov-2017#request-message-
 			 */
-			,'Method' => 'CC'
+			//,'Method' => 'CC'
 		]], df_class_l($this)); /** @var Op $r */
+		echo df_json_encode($r->req());
+		echo $r->j();
 	}
 }
--- T/CaseT/BankCard/RefundPayment.php
+++ T/CaseT/BankCard/RefundPayment.php
@@ -10,10 +10,10 @@ use Dfe\AlphaCommerceHub\API\Facade as F;
  * Note 2. "A `RefundPayment` API request, and a response to it" https://mage2.pro/t/5077
  */
 final class RefundPayment extends \Dfe\AlphaCommerceHub\T\CaseT {
-	/** 2017-12-06 */
+	/** @test 2017-12-06 */
 	function t00() {}
 
-	/** @test 2017-12-06 */
+	/** 2017-12-06 */
 	function t01() {
 		/** @var Op $r */
 		$r = F::s()->post(['Transaction' => ['MerchantTxnID' => '1206L.763']], df_class_l($this));
