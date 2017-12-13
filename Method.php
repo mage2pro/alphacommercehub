400706d10e8baee0a494a4fb74ecf3ce030213c5
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-12-13 05:00:00 +0300

    0.7.2: https://github.com/mage2pro/alphacommercehub/issues/83

--- Method.php
+++ Method.php
@@ -33,6 +33,13 @@ final class Method extends \Df\PaypalClone\Method {
 	 */
 	function amountFormat($a) {
 		$r = round($a * 1000); /** @var int $r */
+		/**
+		 * 2017-12-13
+		 * "It would be better to always pass `0` as the last digit of a payment
+		 * because otherwise AlphaCommerceHub implicitly makes it `0`":
+		 * https://github.com/mage2pro/alphacommercehub/issues/83
+		 */
+		$r = 10 * round($r / 10);
 		return !$this->test() || !in_array($this->option(), ['CC', null]) ? $r : 1000 * round($r / 1000);
 	}
 
